9772f11d0c0517e73cc28ebdb09c2a2127b8e00e
Osher Bar <osherbar123@gmail.com>
2023-03-26 10:18:58 +0300

    Initial commit

---
+++ r/ViewController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using AppKit;
+using Foundation;
+
+namespace r
+{
+	public partial class ViewController : NSViewController
+		{
+		string userChoice = "null";
+		string pcChoice = "null";
+		int randPcChoise = 0;
+        string[] pcOptions = { "rock", "paper", "scissors" };
+
+
+        public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			// Do any additional setup after loading the view.
+		}
+
+		public override NSObject RepresentedObject {
+			get {
+				return base.RepresentedObject;
+			}
+			set {
+				base.RepresentedObject = value;
+				// Update the view, if already loaded.
+			}
+		}
+
+        public static class RandomNumberGenerator
+        {
+            private static readonly Random rand = new Random();
+
+            public static int GenerateRandomNumber(int min, int max)
+            {
+                return rand.Next(min, max);
+            }
+        }
+
+        partial void PaperPush(NSButton sender)
+        {
+            userChoice = "paper";
+            MainFieldStatus.StringValue = "User choise : Paper";
+        }
+
+        partial void RockPush(NSButton sender)
+        {
+            userChoice = "rock";
+			MainFieldStatus.StringValue = "User choise : Rock";
+        }
+
+        partial void ScissorsPush(NSButton sender)
+        {
+            userChoice = "scissors";
+			MainFieldStatus.StringValue = "User choise : Scissors";
+        }
+
+        partial void StartGamePush(NSButton sender)
+        {
+            pcChoice = pcOptions[RandomNumberGenerator.GenerateRandomNumber(0, 3)];
+            pcRandChoice.StringValue = "Pc choise :" + pcChoice;
+            if (userChoice == "null")
+            {
+                MainFieldStatus.StringValue = "Choose Rock Papaer Or Scissors!";
+            }
+
+			 else if (userChoice == pcChoice)
+			{
+				WinLoseStatus.StringValue = "its a tie";
+			}
+            else if ((userChoice == "rock" && pcChoice == "scissors") ||
+                     (userChoice == "paper" && pcChoice == "rock") ||
+                     (userChoice == "scissors" && pcChoice == "paper"))
+            {
+                WinLoseStatus.StringValue = "You win!";
+            }
+            else
+            {
+                WinLoseStatus.StringValue = "You lose!";
+            }
+
+        }
+    }
+}
---
+++ r/ViewController.designer.cs
@@ -0,0 +1,63 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace r
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		AppKit.NSTextField MainFieldStatus { get; set; }
+
+		[Outlet]
+		AppKit.NSTextField pcRandChoice { get; set; }
+
+		[Outlet]
+		AppKit.NSTextField WinLoseStatus { get; set; }
+
+		[Action ("MainTextField:")]
+		partial void MainTextField (AppKit.NSTextField sender);
+
+		[Action ("PaperPush:")]
+		partial void PaperPush (AppKit.NSButton sender);
+
+		[Action ("RockPush:")]
+		partial void RockPush (AppKit.NSButton sender);
+
+		[Action ("ScissorsPush:")]
+		partial void ScissorsPush (AppKit.NSButton sender);
+
+		[Action ("StartGamePush:")]
+		partial void StartGamePush (AppKit.NSButton sender);
+
+		[Action ("StartPush:")]
+		partial void StartPush (AppKit.NSButton sender);
+
+		[Action ("WinLoseField:")]
+		partial void WinLoseField (AppKit.NSTextField sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (MainFieldStatus != null) {
+				MainFieldStatus.Dispose ();
+				MainFieldStatus = null;
+			}
+
+			if (pcRandChoice != null) {
+				pcRandChoice.Dispose ();
+				pcRandChoice = null;
+			}
+
+			if (WinLoseStatus != null) {
+				WinLoseStatus.Dispose ();
+				WinLoseStatus = null;
+			}
+		}
+	}
+}
